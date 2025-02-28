8a638ad1b8f52ecc3e30bb9f07a36ed010c40ada
Sergeyt2000 <sergeyt2000@gmail.com>
2025-02-28 16:01:50 +0200

    JS 09 done

---
+++ src/js/2-form.js
@@ -0,0 +1,42 @@
+const formData = { email: '', message: '' };
+const formEl = document.querySelector('.feedback-form');
+const LS_KEY = 'feedback-form-state';
+
+savedForm();
+
+formEl.addEventListener('submit', onSubmit);
+formEl.addEventListener('input', onInput);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const form = evt.currentTarget;
+  const { email, message } = form.elements;
+  if (email.value === '' || message.value === '') {
+    alert('Fill please all fields');
+  } else {
+    formData.email = email.value;
+    formData.message = message.value;
+    console.log(formData);
+    localStorage.removeItem(LS_KEY);
+    form.reset();
+  }
+}
+
+function onInput(evt) {
+  const { name, value } = evt.target;
+  if (name in formData) {
+    formData[name] = value;
+    localStorage.setItem(LS_KEY, JSON.stringify(formData));
+  }
+}
+
+function savedForm() {
+  const savedData = JSON.parse(localStorage.getItem(LS_KEY));
+  if (!savedData) {
+    return;
+  }
+//   formData.email = savedData.email;
+//   formData.message = savedData.message;
+  formEl.elements.email.value = savedData.email;
+  formEl.elements.message.value = savedData.message;
+}
